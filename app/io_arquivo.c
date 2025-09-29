cd3d6abfffb1b9cad7b312f97371e31c415f3c7b
Theo Espósito <thesposito.estudos@gmail.com>
2025-09-29 16:58:04 +0000

    Leitura e escrita dos arquivos está funcionando

--- app/io_arquivo.c
+++ app/io_arquivo.c
@@ -1,57 +1,60 @@
 #include <stdio.h>
 #include <string.h>
-#define TAM_MAX_STR 20 
+#define TAM_MAX_STR 20
 
-int lerArquivo(char[] path_entrada, char[1000][TAM_MAX_STR] arr_gravacao) {
+int lerArquivo(char path_entrada[50], char arr_gravacao[1000][TAM_MAX_STR]) {
     FILE *entrada = fopen(path_entrada, "r");
     if(entrada == NULL) {
         printf("Erro na abertura do arquivo de entrada! Confira o path: ");
-        printf(path_entrada);
+        printf("%s", path_entrada);
         printf("\n");
+        
         return -1;
     }
 
-    for(int i=0; i<1000; i++) {
-        char[TAM_MAX_STR] palavra;
+    for (int i = 0; i < 1000; i++){
+        char palavra[TAM_MAX_STR];
 
-        // É preciso colocar o \0 aqui??? 
-        fgets(palavra, TAM_MAX_STR, entrada);
-
-        if(palavra == null){
+        // Vou manter o \n no final das strings porque vamos precisar dele depois
+        if (fgets(palavra, TAM_MAX_STR, entrada) == NULL){
             printf("Erro ao ler uma palavra em %s", path_entrada);
             return -2;
         }
-
-        arr_gravacao[i] = palavra;
+        strcpy(arr_gravacao[i], palavra);
     }
 
     fclose(entrada);
     return 0;
 }
 
-int gravarArquivo(char path_saida[], char arr_ordenado[1000][TAM_MAX_STR]) {
+int gravarArquivo(char path_saida[], char arr_ordenado[1000][TAM_MAX_STR]){
     FILE *saida = fopen(path_saida, "w");
-    
-    if(saida == NULL) {
+    if (saida == NULL) {
         printf("Erro na abertura do arquivo de saída! Confira o path: ");
-        printf(path_saida);
+        printf("%s", path_saida);
         printf("\n");
 
         return -1;
     }
 
-    
-    for(int i = 0; i<1000; i++){
-        fprintf(saida,"%s\n",arr_ordenado[i]);
+    for (int i = 0; i < 1000; i++){
+        fprintf(saida, "%s", arr_ordenado[i]);
     }
+    return 0;
 }
 
-
 int main() {
-    // Testando leitura de in:
-    char in[] = "/home/theo-esposito/VSCodeProjects/sorting-comparison/io/in.txt";
-    char arr [TAM_MAX_STR][1000];
+// Testando leitura de in:
+    char in[] = "/workspaces/sorting-comparison/io/in.txt";
+    char arr[1000][TAM_MAX_STR];
 
-    lerArquivo(in, )
+    lerArquivo(in, arr);
+
+    for (int i = 0; i < 1000; i++){
+        printf("%s", arr[i]);
+    }
+
+    gravarArquivo("/workspaces/sorting-comparison/io/test.txt", arr);
+
     return 0;
 }
